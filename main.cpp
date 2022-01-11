7a75d39b39d1517e02c1118dd08cb5f9fdb04f74
SaidArya <saidarya@student.telkomuniversity.ac.id>
2022-01-11 21:27:11 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,12 +13,17 @@ int main(){
 	while (selectMenu != -1 && (P == NULL && !login)) {
 		loginMenu(List, P, login);
 			while (login == true) {
-				cout << "1. Tambah Event" << endl;
+				cout << "\n1. Tambah Event" << endl;
 				cout << "2. registasi event" << endl;
 				cout << "3. hapus event" << endl;
 				cout << "4. tampilkan event yang masih tersedia" << endl;
 				cout << "5. Batalkan registrasi event" << endl;
+				cout << "6. Check In Event" << endl;
+				cout << "7. Tutup registasi event" << endl;
+				cout << "8. Tampilkan tiket" << endl;
 				cout << "9. tampilkan semua data event" << endl;
+				cout << "10. menampilkan semua event dan jumlah peserta yang sudah checkin" << endl;
+				cout << "11. cari peserta di event" << endl;
 				cout << "0. logout" << endl;
 				cout << "Input : "; selectMenu = menu(List, P);
 				if (selectMenu == 0) {
